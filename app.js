7b9db4a61713f4d9ab43d7e3c238c94e9409f483
prisha-pemal <chetanp1618@gmail.com>
2026-06-13 12:37:39 +0000

    mongo add for local

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 mongoose.connect(
-'mongodb://mongodb:27017/userdb'
+'mongodb://localhost:27017/userdb'
 );
 
 const userRoute = require('./routes/user');
