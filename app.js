1f8b1ea5f813e50cf7170443011c29a013ef882a
prisha-pemal <chetanp1618@gmail.com>
2026-06-14 03:55:23 +0000

    Move Jenkinsfile to project root

--- app.js
+++ app.js
@@ -8,18 +8,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect(
+// MongoDB Connection
 mongoose.connect('mongodb://mongodb:27017/mydb')
-);
+.then(() => {
+    console.log('MongoDB Connected');
+})
+.catch((err) => {
+    console.error('MongoDB Connection Error:', err);
+});
 
 const userRoute = require('./routes/user');
 
 app.use('/', userRoute);
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Running on port 3000');
 });
